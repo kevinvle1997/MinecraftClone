92e0a176f194f2a45db9ca29a8bb3981ebe2d17f
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2022-01-19 09:53:01 +0530

    Better terrain + gitignore

--- perlin.py
+++ perlin.py
@@ -12,8 +12,11 @@ freq3 = 20
 
 def noise(nx, ny):
     # Rescale from -1.0:+1.0 to 0.0:1.0
-    return gen.noise2(nx, ny) / 2.0 + 0.5
+    return gen.noise2(nx / 10, ny / 10) / 3.0 + 0.5 * 10
 
+def lerp(t, a, b):
+    """Linear interpolation between a and b, given a fraction t."""
+    return a + t * (b - a)
 
 elevation = []
 for y in range(length):
@@ -21,7 +24,4 @@ for y in range(length):
     for x in range(width):
         nx = x / width - 0.5
         ny = y / length - 0.5
-        elevation[y][x] = 1 * noise(nx * freq1, ny * freq1) + .33 * noise(nx * freq2, ny * freq2) + \
-            .25 * noise(nx * freq3, ny * freq3)
-
-print(elevation)
+        elevation[y][x] = lerp(noise(nx * freq1, ny * freq1), noise(nx * freq2, ny * freq2), noise(nx * freq3, ny * freq3))
